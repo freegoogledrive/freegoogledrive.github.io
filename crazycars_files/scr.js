d8586ca10ddf082b4de381ae28eba9779261eaec
WalkerTownesGYAT <124015917+WalkerTownesGYAT@users.noreply.github.com>
2023-11-04 10:42:21 -0400

    replaced some links with direct references

--- crazycars_files/scr.js
+++ crazycars_files/scr.js
@@ -2,7 +2,7 @@
    m[i].l=1*new Date();
    for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-   (window, document, "script", "https://cdn.jsdelivr.net/npm/yandex-metrica-watch/tag.js", "ym");
+   (window, document, "script", "crazycars_files/tag.js", "ym");
 
    ym(93577277, "init", {
         clickmap:true,
